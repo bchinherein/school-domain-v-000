5ae388b9c9eb359e6232a57062463cc5d92ebee7
Brandon Chin <brandonchin@me.com>
2018-03-05 01:31:51 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,15 @@
-# code here!
+class School
+
+  def initialize(name)
+    @name = name
+    def roster
+      roster = {}
+    end #def roster
+  end #def initialize
+
+  def add_student(grade_level, )
+    roster[grade_level] = []
+    roster[grade_level] <<
+
+  end #def add_student
+end
