fe227e014c57c2b3e92b2e6fe35459ac9750074e
Brandon Chin <brandonchin@me.com>
2018-03-05 02:17:24 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,6 +7,7 @@ class School
   end #def initialize
 
   def add_student(student, grade_level)
+    if @roster.include?(grade_level) == true
     @roster[grade_level] = []
     @roster[grade_level] << student
   end #def add_student
